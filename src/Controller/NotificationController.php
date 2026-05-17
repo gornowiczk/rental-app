df87f1113e6add16a291c3a5de3340d2c577aed5
gornowiczk <115422983+gornowiczk@users.noreply.github.com>
2026-05-17 21:51:10 +0200

    Secure notification read routes

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -6,6 +6,7 @@ use App\Entity\Notification;
 use App\Repository\NotificationRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -28,11 +29,15 @@ class NotificationController extends AbstractController
     }
 
     #[IsGranted('ROLE_USER')]
-    #[Route('/{id}/read', name: 'app_notification_read')]
-    public function markAsRead(Notification $notification, EntityManagerInterface $em): Response
+    #[Route('/{id}/read', name: 'app_notification_read', requirements: ['id' => '\d+'], methods: ['POST'])]
+    public function markAsRead(Request $request, Notification $notification, EntityManagerInterface $em): Response
     {
         if ($notification->getUser() !== $this->getUser()) {
             throw $this->createAccessDeniedException();
+        }
+
+        if (!$this->isCsrfTokenValid('notification_read_' . $notification->getId(), (string) $request->request->get('_token'))) {
+            throw $this->createAccessDeniedException('Bledny token.');
         }
 
         $notification->markAsRead();
@@ -40,11 +45,30 @@ class NotificationController extends AbstractController
 
         return $this->redirectToRoute('app_notifications');
     }
-    #[Route('/notifications/{id}/open', name: 'app_notification_open')]
+
+    #[IsGranted('ROLE_USER')]
+    #[Route('/read-all', name: 'app_notification_read_all', methods: ['POST'])]
+    public function markAllAsRead(Request $request, NotificationRepository $repo, EntityManagerInterface $em): Response
+    {
+        if (!$this->isCsrfTokenValid('notification_read_all', (string) $request->request->get('_token'))) {
+            throw $this->createAccessDeniedException('Bledny token.');
+        }
+
+        foreach ($repo->findBy(['user' => $this->getUser(), 'isRead' => false]) as $notification) {
+            $notification->markAsRead();
+        }
+
+        $em->flush();
+
+        return $this->redirectToRoute('app_notifications');
+    }
+
+    #[IsGranted('ROLE_USER')]
+    #[Route('/{id}/open', name: 'app_notification_open', requirements: ['id' => '\d+'], methods: ['GET'])]
     public function open(Notification $notification, EntityManagerInterface $em): Response
     {
         if ($notification->getUser() !== $this->getUser()) {
-            throw $this->createAccessDeniedException('Nie masz uprawnień do tej akcji.');
+            throw $this->createAccessDeniedException('Nie masz uprawnien do tej akcji.');
         }
 
         if (!$notification->isRead()) {
@@ -60,6 +84,4 @@ class NotificationController extends AbstractController
 
         return $this->redirectToRoute('app_notifications');
     }
-
-
 }
