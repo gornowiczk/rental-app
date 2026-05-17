088bf5ca409af7a0cf9d79afa2ddad9742588934
gornowiczk <115422983+gornowiczk@users.noreply.github.com>
2026-05-17 21:52:28 +0200

    Remove generated SQL dump

--- doctrine_migration_20251215170116.sql
+++
@@ -1,7 +0,0 @@
--- Doctrine Migration File Generated on 2025-12-15 17:01:16
-
--- Version DoctrineMigrations\Version20250216114651
-ALTER TABLE car CHANGE owner_id owner_id INT NOT NULL;
-CREATE UNIQUE INDEX UNIQ_773DE69DFCFF3785 ON car (plate_number);
--- Version DoctrineMigrations\Version20250216114651 update table metadata;
-INSERT INTO doctrine_migration_versions (version, executed_at, execution_time) VALUES ('DoctrineMigrations\\Version20250216114651', '2025-12-15 17:01:16', 0);
