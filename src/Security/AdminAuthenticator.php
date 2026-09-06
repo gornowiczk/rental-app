4aefc161af77a1e5bc198c2796ee4e915433f174
gornowiczk <115422983+gornowiczk@users.noreply.github.com>
2026-09-06 19:26:32 +0200

    Clean up informal code comments

--- src/Security/AdminAuthenticator.php
+++ src/Security/AdminAuthenticator.php
@@ -4,7 +4,7 @@ namespace App\Security;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response; // ← UPEWNIJ SIĘ, ŻE TO JEST DODANE
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
