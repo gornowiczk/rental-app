e4de5938a42fcbb2973430a47b75bb45a957d293
gornowiczk <115422983+gornowiczk@users.noreply.github.com>
2026-09-06 19:46:22 +0200

    Fix AdminLogger Security deprecation

--- src/Service/AdminLogger.php
+++ src/Service/AdminLogger.php
@@ -6,7 +6,7 @@ use App\Entity\AdminLog;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\Security\Core\Security;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class AdminLogger
 {
