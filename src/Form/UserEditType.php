3de94a1f504903079c9c970377cc85f92184158b
gornowiczk <115422983+gornowiczk@users.noreply.github.com>
2026-09-06 19:40:17 +0200

    Fix UserEditType Symfony deprecations

--- src/Form/UserEditType.php
+++ src/Form/UserEditType.php
@@ -13,7 +13,7 @@ use App\Entity\User;
 
 class UserEditType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('firstName', TextType::class, [
@@ -51,7 +51,7 @@ class UserEditType extends AbstractType
             ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => User::class,
